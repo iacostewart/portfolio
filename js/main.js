b8e863e418b89acdd4a92921efa9564e9f4e1080
Ian Stewart <iacostewart@msn.com>
2018-01-22 16:50:08 -0500

    add

--- js/main.js
+++ js/main.js
@@ -35,3 +35,17 @@ jQuery(document).ready(function ($) {
       },
   });
 });
+// Setting up the Variables
+var bars = document.getElementById("nav-action");
+var nav = document.getElementById("nav");
+
+
+//setting up the listener
+bars.addEventListener("click", barClicked, false);
+
+
+//setting up the clicked Effect
+function barClicked() {
+  bars.classList.toggle('active');
+  nav.classList.toggle('visible');
+}
